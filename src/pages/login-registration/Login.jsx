d978f46d1f80f62c259092678f244ce33df8fd91
Tashdik <s2110477153@ru.ac.bd>
2024-08-15 11:24:12 +0600

    login done

--- src/pages/login-registration/Login.jsx
+++ src/pages/login-registration/Login.jsx
@@ -1,5 +1,55 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../auth/AuthProvider";
+import { useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const Login = () => {
+
+    useEffect(() => {
+        window.scroll(0, 0);
+    }, []);
+
+    const { loginUser, googleLogin } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const page = location?.state || '/';
+    const handelLogin = e => {
+        e.preventDefault();
+
+        const form = e.target;
+
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password);
+
+        loginUser(email, password)
+            .then(result => {
+                console.log(result.user);
+                navigate(page);
+
+                toast.success('Login Successful')
+            })
+            .catch(err => {
+                console.log(err);
+                
+                toast.error('auth/invalid-credential');
+            })
+    }
+
+    const handelGoogle = () => {
+        googleLogin()
+            .then(result => {
+                navigate(page);
+                toast.success('Login Successful')
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+
     return (
         <div className="min-h-screen flex items-center justify-center">
             <div className="w-full max-w-sm border border-gray-600 overflow-hidden bg-white rounded-lg shadow-md ">
@@ -12,17 +62,17 @@ const Login = () => {
 
                     <p className="mt-1 text-center text-gray-500 ">Please SignIn</p>
 
-                    <form>
+                    <form onSubmit={handelLogin}>
                         <div className="w-full mt-4">
-                            <input className="block w-full px-4 py-2 mt-2 text-gray-700  bg-white border rounded-lg  border-gray-600 placeholder-gray-400 focus:border-blue-400  focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address" />
+                            <input className="block w-full px-4 py-2 mt-2 text-gray-700  bg-white border rounded-lg  border-gray-600 placeholder-gray-400 focus:border-blue-400  focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" name="email" aria-label="Email Address" />
                         </div>
 
                         <div className="w-full mt-4">
-                            <input className="block w-full px-4 py-2 mt-2 text-gray-700  bg-white border rounded-lg  border-gray-600 placeholder-gray-400 focus:border-blue-400  focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" />
+                            <input className="block w-full px-4 py-2 mt-2 text-gray-700  bg-white border rounded-lg  border-gray-600 placeholder-gray-400 focus:border-blue-400  focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" name="password" aria-label="Password" />
                         </div>
 
                         <div className="flex items-center justify-between mt-4">
-                            <a href="#" className="text-sm text-gray-600  hover:text-gray-500">Forget Password?</a>
+                            <button onClick={handelGoogle}>Google</button>
 
                             <button type="submit" className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                                 Sign In
