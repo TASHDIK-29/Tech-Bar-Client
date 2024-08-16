ca4562da9a5b2e42eff83c5a93606a30ad3ca7ed
Tashdik <s2110477153@ru.ac.bd>
2024-08-16 23:48:19 +0600

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,57 @@
+
+const Footer = () => {
+    return (
+        <footer className="bg-slate-600 text-white">
+            <div className="container px-6 py-12 mx-auto">
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
+                    <div className="sm:col-span-2">
+                        <h1 className="max-w-lg text-xl font-semibold tracking-tight text-gray-800 xl:text-2xl ">Subscribe our newsletter to get update.</h1>
+
+                        <div className="flex flex-col mx-auto mt-6 space-y-3 md:space-y-0 md:flex-row">
+                            <input id="email" type="text" className="px-4 py-2 text-gray-700 bg-white border rounded-md   border-gray-600 focus:border-blue-400  focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Email Address" />
+
+                                <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wider  transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-200 text-black rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
+                                    Subscribe
+                                </button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <p className="font-semibold text-white ">Brands</p>
+
+                        <div className="flex flex-col items-start mt-5 space-y-2">
+                            <a href="#" className="text-white transition-colors duration-300  hover:text-white hover:underline ">Apple</a>
+                            <a href="#" className="text-white transition-colors duration-300  hover:text-white hover:underline ">Samsung</a>
+                            <a href="#" className="text-white transition-colors duration-300  hover:text-white hover:underline ">Google</a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <p className="font-semibold text-white ">Categories</p>
+
+                        <div className="flex flex-col items-start mt-5 space-y-2">
+                            <a href="#" className="text-white transition-colors duration-300  hover:text-white hover:underline ">Laptop</a>
+                            <a href="#" className="text-white transition-colors duration-300  hover:text-white hover:underline ">Mobile</a>
+                            <a href="#" className="text-white transition-colors duration-300  hover:text-white hover:underline ">Tab</a>
+                            <a href="#" className="text-white transition-colors duration-300  hover:text-white hover:underline ">Computer</a>
+                        </div>
+                    </div>
+                </div>
+
+                <hr className="my-6  md:my-8 border-gray-700" />
+
+                    <div className="flex items-center justify-between">
+                        <a href="#">
+                            <img className="w-auto h-7" src="https://merakiui.com/images/full-logo.svg" alt="" />
+                        </a>
+
+                        <div className="flex -mx-2">
+                            
+                        </div>
+                    </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Nav";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar />
-            <div className="container mx-auto">
+            <div className="container mx-auto mb-20 min-h-[50vh]">
                 <Outlet />
             </div>
+            <Footer />
         </div>
     );
 };
